8ddb8c534c8c7dfc6c4e7d5a45b33c02ce5db6ae
OvershotHavik <plavetzky@gmail.com>
2022-02-22 13:27:22 -0500

    Added the Follower and User models

---
+++ GitHub Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub Followers
+//
+//  Created by Steve Plavetzky on 2/22/22.
+//
+
+import Foundation
+
+struct Follower: Codable{
+    var login: String
+    var avatarUrl: String
+}
