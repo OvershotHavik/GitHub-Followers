8532fdca0b527181810d49cd1cea5204f09a33c5
OvershotHavik <plavetzky@gmail.com>
2022-02-22 09:58:12 -0500

    initial setup of the tab bar in scene delegate

---
+++ GitHub Followers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GitHub Followers
+//
+//  Created by Steve Plavetzky on 2/22/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GitHub Followers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GitHub Followers
+//
+//  Created by Steve Plavetzky on 2/22/22.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
