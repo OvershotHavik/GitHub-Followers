cffc76da1a84804cbf7cc32c5e4b3d3e8759f00d
OvershotHavik <plavetzky@gmail.com>
2022-02-22 13:00:42 -0500

    Organizing project, and refactored the container view to it's own view file

---
+++ GitHub Followers/Custom Views/GFAlertContainerView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAlertContainerView.swift
+//  GitHub Followers
+//
+//  Created by Steve Plavetzky on 2/22/22.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
