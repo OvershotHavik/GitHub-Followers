a6029d17f7f030b98d20713555d0df4e7f97b289
OvershotHavik <plavetzky@gmail.com>
2022-02-22 16:30:49 -0500

    Refactored the followers List VC to remove the flow layout

--- GitHub Followers/Screens/FollowersListVC.swift
+++ GitHub Followers/Screens/FollowersListVC.swift
@@ -15,7 +15,6 @@ class FollowersListVC: UIViewController {
     var username: String!
     var followers: [Follower] = []
     
-    
     var collectionView: UICollectionView!
     var dataSource : UICollectionViewDiffableDataSource<Section, Follower>!
     
@@ -40,30 +39,17 @@ class FollowersListVC: UIViewController {
     
     
     func configureCollectionView(){
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
     
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout{
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
-    }
-    
-    
     func getFollowers(){
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) {[weak self] result in
+            guard let self = self  else {return}
+            
             switch result{
             case .success(let followers):
                 print("Followers count: \(followers.count)")
@@ -85,6 +71,7 @@ class FollowersListVC: UIViewController {
         })
     }
     
+    
     func updateData(){
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
@@ -92,6 +79,5 @@ class FollowersListVC: UIViewController {
         DispatchQueue.main.async {
             self.dataSource.apply(snapshot, animatingDifferences: true)
         }
-        
     }
 }
