468d3b383f0752f57d4b39942f9613ae1135a26f
OvershotHavik <plavetzky@gmail.com>
2022-02-23 15:47:10 -0500

    Moved Download image to the network manager

--- GitHub Followers/Managers/NetworkManager.swift
+++ GitHub Followers/Managers/NetworkManager.swift
@@ -94,6 +94,32 @@ class NetworkManager {
         }
         task.resume()
     }
+    
+    
+
+    func downloadImage(from url: String, completion: @escaping (UIImage?) -> Void){
+        let cacheKey = NSString(string: url)
+        if let image = cache.object(forKey: cacheKey){
+            completion(image)
+            return
+        }
+        guard let url = URL(string: url) else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
+            guard let self = self,
+                  error == nil,
+                  let response = response  as? HTTPURLResponse, response.statusCode == 200,
+                  let data = data,
+                  let image = UIImage(data: data) else {
+                      completion(nil)
+                      return
+                  }
+            self.cache.setObject(image, forKey: cacheKey)
+            completion(image)
+            
+        }
+        task.resume()
+    }
 }
 
 
