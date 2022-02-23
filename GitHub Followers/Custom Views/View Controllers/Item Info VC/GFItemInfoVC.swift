d7f828020730f61b711e12b0025190edaaa9f720
OvershotHavik <plavetzky@gmail.com>
2022-02-23 11:39:01 -0500

    UserInfoVC now setup with all he info populating with he UI

--- GitHub Followers/Custom Views/View Controllers/Item Info VC/GFItemInfoVC.swift
+++ GitHub Followers/Custom Views/View Controllers/Item Info VC/GFItemInfoVC.swift
@@ -13,7 +13,7 @@ class GFItemInfoVC: UIViewController {
     let itemInfoViewOne = GFItemInfoView()
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton = GFButton()
-    
+    var user: User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +22,14 @@ class GFItemInfoVC: UIViewController {
         configureStackView()
     }
     
+    init(user: User){
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
      private func configureBackgroundView(){
         view.layer.cornerRadius = 18
